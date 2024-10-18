cab8bf664e5f74bbad7f4b9949bd60735c7c995b
2123063YoheiNoro <s2123063@cco.kanagawa-it.ac.jp>
2024-10-18 10:26:52 +0900

    大学での作業を追加 麻雀関連のスクリプト周りの実装を行った

---
+++ Assets/Scripts/Mock_TileData.cs
@@ -0,0 +1,42 @@
+//**************************************************
+//テスト用の麻雀牌認識結果を提供するクラス
+//**************************************************
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using static DynamicSceneManagerHelper.SceneSnapshot;
+
+public class Mock_TileData : MonoBehaviour, IPredictions
+{
+    private Predictions _predictions;
+
+    List<Prediction> IPredictions.GetAllPredictions()
+    {
+        if (_predictions == null)
+        {
+            Debug.LogError("predctionsの中身がありません");
+        }
+        return _predictions.GetAllPredictions();
+    }
+
+    public Mock_TileData(string jsonData)
+    {
+        _predictions = Convert_Json_To_Predictions(jsonData);
+    }
+
+    private Predictions Convert_Json_To_Predictions(string jsonData)
+    {
+        //「class」という変数名が使えないため「class_name」に置換する.
+        string fixedText = jsonData.Replace("class", "class_name");
+        //上の変換で「class_id」が「class_name_id」に変わってしまうので修正する.
+        fixedText = fixedText.Replace("class_name_id", "class_id");
+
+        Debug.Log(jsonData);
+        Debug.Log(fixedText);
+        //オブジェクトに変換.
+        Predictions predictions = JsonUtility.FromJson<Predictions>(fixedText);
+        return predictions;
+    }
+}
