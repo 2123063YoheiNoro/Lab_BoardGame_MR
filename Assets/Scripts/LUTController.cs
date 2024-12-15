0fb8359e6ffb377854e84cc8572b26e5dbb2dfe2
yohei-noro <s2123063@cco.kanagawa-it.ac.jp>
2024-12-15 16:09:40 +0900

    牌と点棒のモデルを一つのマテリアルのメッシュに変更した。LUTの滑らかな変化を実装した。DepthAPIの準備を行った。

--- Assets/Scripts/LUTController.cs
+++ Assets/Scripts/LUTController.cs
@@ -9,13 +9,16 @@ using UnityEngine.AddressableAssets;
 public class LUTController : MonoBehaviour
 {
     private Material _lutMaterial;
-    private const string _weightPropertyName = "_weight";
-    private const string _lutPropertyName = "_LUTTexture";
+    private const string _weightPropertyName = "_weight";   //weightの変数名
+    private const string _lutPropertyName = "_LUTTexture";  //lutテクスチャの変数名
     private OVRPassthroughLayer _ovrPassthroughLayer;
-    [SerializeField] private float _weight = 0;
-    [SerializeField] private Texture2D _defaultLut;
 
-    OVRPassthroughColorLut _latestLut;  //LUTの強さ単体では変更できないので現在のLUTを記憶しておく.
+    [SerializeField] private float _weight = 0; //LUTの強さ.
+    [SerializeField] private Texture2D _defaultLut; //初期のLUT.
+
+    private OVRPassthroughColorLut _latestLut;  //LUTの強さ単体では変更できないので現在のLUTを記憶しておく.
+    private Coroutine _latestCoroutine; //weightの変化に使うコルーチン(停止用)
+    [SerializeField] private float ChangeSpeed = 1; //weightの変化の早さ
 
     private void Start()
     {
@@ -33,6 +36,7 @@ public class LUTController : MonoBehaviour
     public void SetLut(Texture2D lutTex, float weight)
     {
         weight = Mathf.Clamp01(weight);
+        _weight = weight;
         OVRPassthroughColorLut ovrLut;
         //新しいテクスチャが無かったら最後に適用したLUTを設定する.
         if (lutTex == null)
@@ -48,7 +52,7 @@ public class LUTController : MonoBehaviour
         }
         else
         {
-            ovrLut = new OVRPassthroughColorLut(lutTex,false);
+            ovrLut = new OVRPassthroughColorLut(lutTex, false);
             _latestLut = ovrLut;
         }
 
@@ -68,7 +72,40 @@ public class LUTController : MonoBehaviour
             return;
         }
         weight = Mathf.Clamp01(weight);
+        _weight = weight;
         _ovrPassthroughLayer.SetColorLut(_latestLut, weight);
         _lutMaterial.SetFloat(_weightPropertyName, weight);
+    }
+
+    /// <summary>
+    /// lutの強さを滑らかに変化させる.
+    /// </summary>
+    /// <param name="weight"></param>
+    public void SetLutWeightSmooth(float weight)
+    {
+        if (_latestCoroutine != null)
+        {
+            StopCoroutine(_latestCoroutine);
+        }
+
+        StartCoroutine(SmoothChange(weight));
+    }
+
+    private IEnumerator SmoothChange(float targetValue)
+    {
+        targetValue = Mathf.Clamp01(targetValue);
+
+        //weightを線形補間でゆっくり動かす.
+        while (Mathf.Abs(_weight - targetValue) > 0.1f)
+        {
+            _weight = Mathf.Lerp(_weight, targetValue, Time.deltaTime * ChangeSpeed);
+            SetLutWeight(_weight);
+            yield return null;
+        }
+
+        SetLutWeight(targetValue);
+
+        //yield returnが発生しない可能性があるのでここで実行.
+        yield return null;
     }
 }
